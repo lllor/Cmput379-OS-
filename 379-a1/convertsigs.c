6e69868b3243b196773e5b4af9b82ad1de34ecda
lanfang Zhang <lllorenaz123@gmail.com>
2019-01-24 11:10:59 -0700

    '?' appeared in second line and so on

--- 379-a1/convertsigs.c
+++ 379-a1/convertsigs.c
@@ -66,22 +66,15 @@ char convertBack(char a[] ) { // a have to be an array with 8 element
 
 #else
   void send(char message[], int length, pid_t pid) {
-    int count=0;
-    //printf("%s\n",message);
     for(int i = 0; i < length; i++) {
-      //printf("%c\n",message[i]);
       char sendChar[8];
       convert(message[i], sendChar);
       for(int a = 0; a < 8; a++) {
         if(sendChar[a] == '0') {
-          //printf("Send Siguser2,%d\n",counter);
           kill(pid, SIGUSR2);
-          //counter++;
         }
         else {
-          //printf("Send siguser1,%d\n",counter);
           kill(pid, SIGUSR1);
-          //counter++;
         }
         //sleep(1);
         usleep(50000);
@@ -89,56 +82,52 @@ char convertBack(char a[] ) { // a have to be an array with 8 element
     }
     
     for(int a = 0; a<8 ; a++){
-      //printf("Send siguser1 for end,%d\n",counter);
       kill(pid,SIGUSR1);
+      usleep(50000);
     }
     
     
     
   }
   void handler(int signal_val) {
-    //printf("%d",counter);
     switch(signal_val){
       case SIGUSR1:
-        //printf("Recieved SIGUSR1,%d\n",counter);
+        printf("Recieved SIGUSR1,%d\n",counter);
         addBack(buffer,'1');
         break;
       case SIGUSR2:
-        //printf("Recieved SIGUSR2,%d\n",counter);
+        printf("Recieved SIGUSR2,%d\n",counter);
         addBack(buffer,'0');
         break;
     }
     //sleep(1);
     counter++;
+  
+    if (counter%8 == 0){
+      counter -= 8;
+      char temp[8];
+      for(int i = 0; i < 8; i++) {
+        temp[i] = peekFront(buffer);
+        leaveFront(buffer);
+      }
+      
+      if (temp[0] == '1' && temp[1] == '1' && temp[2] == '1' && temp[3] == '1'
+       && temp[4] == '1' && temp[5] == '1' && temp[6] == '1' && temp[7] == '1') {
+        print(message,'f');
+        nuke(buffer);
+        nuke(message);
+        print(message,'f');
+      }
+      
+      addBack(message,convertBack(temp));
+
+    }
+  
   }
 #endif
 
 
-int receive() {
- // print(buffer,'f');
-  //printf("receive\n");
-  //leaveFront(buffer);
-  
-  
-  char flag[8]={'1','1','1','1','1','1','1','1'};
-  while(counter%8 == 0) {
-    counter -= 8;
-    char temp[8];
-    
-    for(int i = 0; i < 8; i++) {
-      temp[i] = peekFront(buffer);
-      leaveFront(buffer);
-    }
-    
-    
-    if (temp[0] == '1' && temp[1] == '1' && temp[2] == '1' && temp[3] == '1'
-     && temp[4] == '1' && temp[5] == '1' && temp[6] == '1' && temp[7] == '1') {
-      return 1;
-    }
-    addBack(message,convertBack(temp));
-  }
-  return 0;
-}
+
 
 int main(void) {
   initSqueue(&buffer);
@@ -158,31 +147,27 @@ int main(void) {
   pid_t otherpid;
   scanf("%d", &otherpid);
   char waste = getchar();
+  
+  
+  
   while(1){
     char input[4096]={0};
     int i = 0;
     char ch;
     
+    counter = 0;
+    
     while((ch = getchar()) != '\n') {
           input[i++] = ch;
     }
     
-    //
-    //printf("%s\n",input);
+    send(input,strlen(input), otherpid);
+    //send("abc",3, otherpid);
     
     
     
-      
-    send(input,strlen(input)+1, otherpid);
-    if(receive()==1){
-      print(message,'f');
-      printf("\n");
-      nuke(message);
-      nuke(buffer);
-    }
-    
-    counter = 0;
-    usleep(50000);
+    //sleep(1);
+    usleep(10000);
     //exit(1);
 
     }
@@ -194,3 +179,24 @@ int main(void) {
   
   
 
+/*int receive() {
+  int count = counter;
+  printf("I'm in\n");
+  while(count%8 == 0 && count != 0) {
+    count -= 8;
+    char temp[8];
+    
+    for(int i = 0; i < 8; i++) {
+      temp[i] = peekFront(buffer);
+      leaveFront(buffer);
+    }
+    
+    
+    if (temp[0] == '1' && temp[1] == '1' && temp[2] == '1' && temp[3] == '1'
+     && temp[4] == '1' && temp[5] == '1' && temp[6] == '1' && temp[7] == '1') {
+      return 1;
+    }
+    addBack(message,convertBack(temp));
+  }
+  return 0;
+}*/
