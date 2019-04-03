4aa93b3fb1d9a92a7aaca8728191ada69bee5d97
CMPUT <cmput274@ualberta.ca>
2019-04-02 18:31:05 -0600

    LRU with linkedlist

--- a3/TLB.c
+++ a3/TLB.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "linklist.h"
+#include <math.h>
 
 int findAddr(unsigned int address, int pageNum[], int length);
 void updatePageNumFIFO(unsigned int address, int *pageNum);
@@ -69,8 +70,6 @@ void dealLRU(int pgsize,int tlbsize,int flushPeriod, int ignoreI){
     //?if "addr" is not in flag_list, add it to flag_list, 
     //add it to the end of TLB
     //if flag_list is full/if the TLB is full, remove the first pagenum from the TLB and add the addr to the end
-}
-void dealFIFO(int pgsize,int tlbsize,int flushPeriod, int ignoreI){
     Linklist *TLB = NULL;
     initLinklist(&TLB);
 
@@ -83,7 +82,14 @@ void dealFIFO(int pgsize,int tlbsize,int flushPeriod, int ignoreI){
     int TLB_size = 0;
     int addr_count = 0;
     int result = 0;
-    
+    int offset = 4;
+
+    for(;offset <= 16; offset++) {
+        if(pow(2, offset) == pgsize) {
+            break;
+        }
+    }
+    printf("%d\n",offset);
     while(fgets(buf,sizeof(buf),stdin)) {
         if(buf[0] == '=') {
             continue;
@@ -101,7 +107,82 @@ void dealFIFO(int pgsize,int tlbsize,int flushPeriod, int ignoreI){
         
         strncpy(addr_str,buf+3,8);
         sscanf(addr_str,"%x",&addr);
-        addr = addr >> pgsize;
+        addr = addr >> offset;
+        
+        struct Node *pp=TLB->first;              //begin with the first element
+        while(pp!=NULL)                          //til the end
+        {
+            if(pp->pagenum == addr){
+                result = 1;
+                break;
+            }
+            pp=pp->next;
+        }
+        
+        
+        if(result != 0) {
+            hit++;
+            removeCurrent(TLB, addr);
+            addBack(TLB,addr);
+        }
+        else {
+            miss++;
+            if (TLB_size >= tlbsize){
+                leaveFront(TLB);
+                addBack(TLB,addr);
+            }
+            else{
+                addBack(TLB,addr);
+            }
+        }
+
+        memset(buf,0,100);
+        result = 0;
+    }
+    printf("%d %d %d\n",addr_count, hit, miss);
+    destroy(&TLB);
+}
+
+
+void dealFIFO(int pgsize,int tlbsize,int flushPeriod, int ignoreI){
+    Linklist *TLB = NULL;
+    initLinklist(&TLB);
+
+    char buf[100] = {0};
+    char addr_str[9] = {0};
+    unsigned int addr;
+    int position = 0;
+    int hit = 0;
+    int miss = 0;
+    int TLB_size = 0;
+    int addr_count = 0;
+    int result = 0;
+    int offset = 4;
+
+    for(;offset <= 16; offset++) {
+        if(pow(2.0,(double)offset) == pgsize) {
+            break;
+        }
+    }
+    printf("%d\n",offset);
+    while(fgets(buf,sizeof(buf),stdin)) {
+        if(buf[0] == '=') {
+            continue;
+        }
+        if(ignoreI == 1) {
+            if(buf[0] == 'I') {
+                continue;
+            }
+        }
+        addr_count += 1;
+        if(addr_count == flushPeriod){
+            nuke(TLB);
+            addr_count = 0;
+        }
+        
+        strncpy(addr_str,buf+3,8);
+        sscanf(addr_str,"%x",&addr);
+        addr = addr >> offset;
         
         struct Node *pp=TLB->first;              //begin with the first element
         while(pp!=NULL)                          //til the end
@@ -119,7 +200,6 @@ void dealFIFO(int pgsize,int tlbsize,int flushPeriod, int ignoreI){
         }
         else {
             miss++;
-            
             if (TLB_size >= tlbsize){
                 leaveFront(TLB);
                 addBack(TLB,addr);
@@ -132,6 +212,6 @@ void dealFIFO(int pgsize,int tlbsize,int flushPeriod, int ignoreI){
         memset(buf,0,100);
         result = 0;
     }
-    printf("%d %d\n",hit, miss);
+    printf("%d %d %d\n",addr_count, hit, miss);
     destroy(&TLB);
 }
